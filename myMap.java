14b2462a63563bf9b15d54e84665a02ac7b2eeb0
DA MAC M1 108 <siyabopngazungu95@gmail.com>
2024-10-16 10:44:14 +0200

    HashMap

---
+++ myMap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class myMap{
+    public static void main(String[] args) {
+        Map<String, Integer> Students = new HashMap<String, Integer>();
+        // WE USE PUT TO ADD SOME VALUES 
+        Students.put("John", 60 );
+        Students.put("Alice", 80 );
+        Students.put("Bob", 75 ); 
+
+        System.out.println("Students: " + Students);
+
+
+        System.out.println(Students.keySet());
+        for(String key: Students.keySet()){
+            System.out.println(key + ": " + Students.get(key));
+        }
+    }
+}
